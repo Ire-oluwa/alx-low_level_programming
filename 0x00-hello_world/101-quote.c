19d9eabe46b83459e81936a4fc3c1944b14689e1
Ire-oluwa <orodiji.ireoluwa@gmail.com>
2022-09-08 04:28:45 -0700

    a C program that prints an error message followed by a new line, to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<unistd.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 1(Success)
+ */
+int main(void)
+{
+char errorMessage[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+write(2, errorMessage, 59);
+return (1);
+}
