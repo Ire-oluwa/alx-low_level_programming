0c5e2336cdb7a0ba893c82bf979e926f969a50f7
Ire-oluwa <orodiji.ireoluwa@gmail.com>
2022-09-09 07:52:25 -0700

    This program will assign a random number to the variable n each time it is executed. Complete the source code in order to print the last digit of the number stored in the variable n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ printf("Last digit of %d is %d and is 0\n", n, l);
 }
 else if (l < 6)
 {
-printf("Last digit of %d is %d and is lesser than 6 and not 0\n", n, l);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 }
 return (0);
 }
