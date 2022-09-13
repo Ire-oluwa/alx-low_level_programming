b3c8d14c3398e0be0f33d57c4f2c52104606caaa
Ire-oluwa <orodiji.ireoluwa@gmail.com>
2022-09-13 06:16:00 -0700

    a function that checks for alphabetic character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,14 +2,14 @@
 
 /**
  * int _isalpha(int c) - checks for uppercase character
- * @c - the character in ASCII code
+ * @c: the character in ASCII code
  *
  * Return: 1 if c is upprecase or lowercase, 0 if otherwise
  */
 
 int _isalpha(int c)
 {
-if ((c >=65 && c<= 90) || (c >= 97 && c <= 122))
+if ((c >= 65 && c<= 90) || (c >= 97 && c <= 122))
 {
 return (1);
 }
