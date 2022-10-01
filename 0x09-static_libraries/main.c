63ed5a50fd8c8614adcd417181fc97464f952f33
Ire-oluwa <orodiji.ireoluwa@gmail.com>
2022-10-01 02:20:08 -0700

    a program that prints a message

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * main - prints a message
+ * Return: 0 for success
+ */
+
 int main(void)
 {
 _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
